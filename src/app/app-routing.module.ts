2b9bfb2772901da4575497708f1e43f676e620a8
Pedro Gonçalves <pegons.log@gmail.com>
2021-12-22 15:49:36 -0300

    Melhorando o routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,40 @@
-import { LoginPageComponent } from './pages/acount/login-page/login-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PetsPageComponent } from './pages/acount/pets-page/pets-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { FramePageComponent } from './pages/master/frame.page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
-import { ResetPasswordPageComponent } from './pages/acount/reset-password-page/reset-password-page.component';
-import { SignUpPageComponent } from './pages/acount/sign-up-page/sign-up-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignUpPageComponent } from './pages/account/sign-up-page/sign-up-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FramePageComponent,
     children: [
-      { path: '', component: ProductsPageComponent },
-      { path: 'cart', component: CartPageComponent },
+      { path: 'produtos', component: ProductsPageComponent },
+      { path: 'cart', component: CartPageComponent }
     ]
   },
   {
     path: 'account',
     component: FramePageComponent,
     children: [
+      { path: 'pets', component: PetsPageComponent },
+    ]
+  },
+  {
+    path: 'store',
+    component: FramePageComponent,
+    children: [
       { path: 'produtos', component: ProductsPageComponent },
+      { path: 'cart', component: CartPageComponent }
     ]
   },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignUpPageComponent },
-  { path: 'reset-password', component: ResetPasswordPageComponent },
+  { path: 'reset-password', component: ResetPasswordPageComponent }
 ];
 
 @NgModule({
